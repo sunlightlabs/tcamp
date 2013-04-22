415d06872e721068e3654fd58ab341279d39f53d
Dan Drinkard <dan.drinkard@gmail.com>
2013-04-22 16:07:08 -0400

    nicer settings gets into request context

--- tcamp/context_processors.py
+++ tcamp/context_processors.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 
+
 def basic_settings(request):
     return {
-        'FAVICON': settings.FAVICON,
-        'APPLE_TOUCH_ICON': settings.APPLE_TOUCH_ICON,
-        'SHARING_IMAGE': settings.SHARING_IMAGE,
-        'FB_APP_ID': settings.FB_APP_ID,
-        'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID,
-        'CACHEBUSTER': settings.CACHEBUSTER,
+        'FAVICON': getattr(settings, 'FAVICON'),
+        'APPLE_TOUCH_ICON': getattr(settings, 'APPLE_TOUCH_ICON'),
+        'SHARING_IMAGE': getattr(settings, 'SHARING_IMAGE'),
+        'FB_APP_ID': getattr(settings, 'FB_APP_ID'),
+        'GOOGLE_ANALYTICS_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID'),
+        'CACHEBUSTER': getattr(settings, 'CACHEBUSTER'),
     }
