0707a9733e0e9bd728424643406dbd93cfafa5f5
Dan Drinkard <dan.drinkard@gmail.com>
2013-04-26 13:40:09 -0400

    toggle scrolling controls on window resize so we don't get in the way of selecting text on desktops

--- tcamp/static/scripts/drawer-menu.js
+++ tcamp/static/scripts/drawer-menu.js
@@ -3,31 +3,32 @@
  * draggable panel for mobile that reveals a menu underneath
  */
 
-// define some utility functions to prevent click bleed-through on the menu trigger
 (function($){
+  // define some utility functions to prevent click bleed-through on the menu trigger
   function freezeLinks(el){
-    $(el).find('a').bind('click.freezelinks', function(e){
+    $(el).find('a').bind('click.drawer-menu', function(e){
       e.preventDefault();
-      e.stopPropagation();
+      e.stopImmediatePropagation();
     });
   }
   function unfreezeLinks(el){
     setTimeout(function(){
-      $(el).find('a').unbind('click.freezelinks');
+      $(el).find('a').unbind('click.drawer-menu');
     }, 10);
   }
+
   // init menu on domready
   $(function(){
     (function(opts){
-      // silly hamburger menu!
       // first, create a couple of containers for the content panel and drawer
       var menuPanel = $('<div class="' + opts.panel.replace('.', '') + '"></div>'),
           menuDrawer = $('<div class="' + opts.drawer.replace('.', '') + '"></div>'),
           // define an event handler to make sure we can't scroll sideways
           verticalScrollOnly = function(e){
             if($(window).width() > opts.phoneWidth ||
-            (e.distX > e.distY && e.distX < -e.distY) ||
-            (e.distX < e.distY && e.distX > -e.distY)) {
+                (e.distX > e.distY && e.distX < -e.distY) ||
+                (e.distX < e.distY && e.distX > -e.distY)) {
+              // allows scroll to happen
               e.preventDefault();
             }
           };
@@ -38,54 +39,71 @@
       // wrap the page with the panel
       $('body').children().wrapAll(menuPanel);
       // hide the drawer so it doesn't show while the page is continuing to load
-      // show it a hacky 500 msecs later.
+      // and then show it a hacky 500 msecs later.
       menuDrawer.css('height', 0);
       setTimeout(function(){ menuDrawer.css('height', '100%'); }, 500);
       // add the drawer to the body
       $('body').append(menuDrawer);
-      // override the bootstrap click behavior for parent nav items
+      // override the bootstrap default click behavior for parent nav items
       $(opts.drawer + ' a.dropdown-toggle').click(function(){
         window.location.href = $(this).attr('href');
       });
-      // enforce scrolling to vertical-only
-      $(opts.drawer + ', ' + opts.panel).on('movestart', verticalScrollOnly);
-      // now bind some swipe handlers
+
+      // capturing the move event prevents scrolling on touch devices.
+      // whenever we dip below the threshold dimensions, bind our movestart to
+      // skip the move event bindings entirely if we're scrolling more vertically
+      // than horizontally.
+      // resize() is triggered on domready in app.js, so there's no need to do it here.
+      $(window).resize($.throttle(150, function(){
+        if($(window).width() < opts.phoneWidth){
+          $(opts.drawer + ', ' + opts.panel).on('movestart.drawer-menu', verticalScrollOnly);
+        }else{
+          $(opts.drawer + ', ' + opts.panel).off('movestart.drawer-menu', verticalScrollOnly);
+        }
+      }));
+
+      // now bind some swipe handlers on the panel using the move event.
       $(opts.panel)
       // as we swipe, calculate the position and distance and move
       // the panel to the corresponding left coordinate on the screen
       .on('move', function(e){
-        e.stopPropagation();
-        var el = $(opts.panel),
-            menu = $(opts.drawer),
-            width = $(window).width();
-        if(e.distX < 0){ // swipe left
-          left = 50 * (e.pageX + e.distX) / width;
-          el.css('left', Math.max(left, 0) + '%');
-        }
-        if(e.distX > 0){ // swipe right
-          left = 100 * (e.distX) / width;
-          el.css('left', Math.min(left, opts.bound) + '%');
+        e.preventDefault();
+        if($(window).width() <= opts.phoneWidth){
+          e.stopPropagation();
+          var el = $(opts.panel),
+              menu = $(opts.drawer),
+              width = $(window).width();
+          if(e.distX < 0){ // swiping left
+            left = 50 * (e.pageX + e.distX) / width;
+            el.css('left', Math.max(left, 0) + '%');
+          }
+          if(e.distX > 0){ // swiping right
+            left = 100 * (e.distX) / width;
+            el.css('left', Math.min(left, opts.bound) + '%');
+          }
         }
       })
       // at the end of the swipe, check to see if we've moved far enough to snap open.
-      // if not snap closed.
+      // if not, snap closed.
       .on('moveend', function(e){
         e.preventDefault();
-        e.stopPropagation();
-        var el = $(opts.panel),
-            menu = $(opts.drawer),
-            width = $(window).width();
-        if(el.offset().left + e.distX > width / 2){
-          el.css('left', opts.bound + '%');
-          menu.css('z-index', 1);
-          unfreezeLinks(opts.drawer);
-        }else{
-          el.css('left', '0');
-          menu.css('z-index', 0);
-          freezeLinks(opts.drawer);
+        if($(window).width() <= opts.phoneWidth){
+          e.stopPropagation();
+          var el = $(opts.panel),
+              menu = $(opts.drawer),
+              width = $(window).width();
+          if(el.offset().left + e.distX > width / 2){
+            el.css('left', opts.bound + '%');
+            menu.css('z-index', 1);
+            unfreezeLinks(opts.drawer);
+          }else{
+            el.css('left', '0');
+            menu.css('z-index', 0);
+            freezeLinks(opts.drawer);
+          }
         }
       });
-      // toggle on clicks of the menu-trigger
+      // also, toggle the menu on clicks of the menu-trigger element.
       $(opts.trigger)
       .click(function(e){
         e.preventDefault();
@@ -93,7 +111,7 @@
         $(opts.panel).css('left',
           $(opts.panel).offset().left === 0 ? '80%' : '0');
       });
-    })({
+    })({  // immediately execute this closure, and pass in some options:
       phoneWidth: 768,
       bound: 80, // as a percent
       trigger: '.menu-trigger',
@@ -102,7 +120,7 @@
       sourceMenu: '#menu'
     });
 
-    // default state is for links in drawer menu to be noops
+    // default state is for links in drawer menu to be no-ops
     freezeLinks('.menu-drawer');
   });
 })(jQuery);
