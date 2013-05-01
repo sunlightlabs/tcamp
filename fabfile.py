3f836a169228b8c01903d0926d8f2ff377e6b91f
Dan Drinkard <dan.drinkard@gmail.com>
2013-05-01 12:13:27 -0400

    no moar sudoz

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,12 @@
 from __future__ import with_statement
 import datetime
-import glob
 
 from fabric.api import *
 from config.fabconfig import *
 
 env.hosts = REMOTE_HOSTS
 env.user = USER
-env.sudo_user = FS_USER
+env.sudo_user = SUDO_USER
 env.key_filename = KEY_FILENAME
 
 
@@ -15,40 +14,40 @@ def setup_clean():
     with prefix('source %s/bin/activate' % VENV_PATH):
         with cd(WORKING_PATH):
             try:
-                sudo("rm -rf %s" % CURRENT_DIR)
+                run("rm -rf %s" % CURRENT_DIR)
             except:
                 pass
             try:
-                sudo("rm -rf %s" % RELEASES_DIR)
+                run("rm -rf %s" % RELEASES_DIR)
             except:
                 pass
             try:
-                sudo("rm -rf %s" % CHECKOUT_DIR)
+                run("rm -rf %s" % CHECKOUT_DIR)
             except:
                 pass
-            sudo("mkdir -p %s %s" % (RELEASES_DIR, SHARED_DIR))
-            sudo("git clone %s %s" % (GIT_URL, CHECKOUT_DIR))
+            run("mkdir -p %s %s" % (RELEASES_DIR, SHARED_DIR))
+            run("git clone %s %s" % (GIT_URL, CHECKOUT_DIR))
     create_release()
     symlink_current()
 
 
 def use_branch():
     with cd("%s/%s" % (WORKING_PATH, CHECKOUT_DIR)):
-        sudo("git checkout %s" % GIT_BRANCH)
+        run("git checkout %s" % GIT_BRANCH)
 
 
 def update_remote_code():
     with cd("%s/%s" % (WORKING_PATH, CHECKOUT_DIR)):
-        sudo("git pull")
+        run("git pull")
 
 
 def create_release():
     release = datetime.datetime.now().isoformat()
     release_dir = "%s/%s" % (RELEASES_DIR, release)
     with cd(WORKING_PATH):
-        sudo("mkdir %s" % release_dir)
-        sudo("cp -Rv %s/* %s/" % (CHECKOUT_DIR, release_dir))
-        sudo("rm -rf %s/.git %s/.gitignore" % (release_dir, release_dir))
+        run("mkdir %s" % release_dir)
+        run("cp -Rv %s/* %s/" % (CHECKOUT_DIR, release_dir))
+        run("rm -rf %s/.git %s/.gitignore" % (release_dir, release_dir))
 
 
 def symlink_current():
@@ -58,31 +57,31 @@ def symlink_current():
         dirs.reverse()
         dir = dirs[0].strip('/')
         try:
-            sudo("unlink %s" % CURRENT_DIR)
+            run("unlink %s" % CURRENT_DIR)
         except:
             pass
-        sudo("ln -nfs %s %s" % (dir, CURRENT_DIR))
+        run("ln -nfs %s %s" % (dir, CURRENT_DIR))
         with cd(dir):
             for path in SHARED_FILES:
-                sudo("ln -nfs %s/%s/%s %s" % (WORKING_PATH, SHARED_DIR,
-                                              path.split('/')[-1],
-                                              path))
+                run("ln -nfs %s/%s/%s %s" % (WORKING_PATH, SHARED_DIR,
+                                             path.split('/')[-1],
+                                             path))
     with cd("%s/bin" % HOME_PATH):
         try:
-            sudo("unlink ./run")
-            sudo("unlink ./stop")
+            run("unlink ./run")
+            run("unlink ./stop")
         except:
             pass
-        sudo("ln -nfs %s/%s/config/run" % (WORKING_PATH, CURRENT_DIR))
-        sudo("ln -nfs %s/%s/config/stop" % (WORKING_PATH, CURRENT_DIR))
+        run("ln -nfs %s/%s/config/run" % (WORKING_PATH, CURRENT_DIR))
+        run("ln -nfs %s/%s/config/stop" % (WORKING_PATH, CURRENT_DIR))
 
 
 def sync_remote_assets():
     with prefix("source %s/bin/activate" % VENV_PATH):
         with cd("%s/%s" % (WORKING_PATH, CURRENT_DIR)):
-            sudo("./manage.py collectstatic --noinput")
+            run("./manage.py collectstatic --noinput")
             try:
-                sudo("./manage.py compress")
+                run("./manage.py compress")
             except:
                 pass
 
@@ -90,30 +89,30 @@ def sync_remote_assets():
 def install_dependencies():
     with prefix("source %s/bin/activate" % VENV_PATH):
         with cd("%s/%s" % (WORKING_PATH, CURRENT_DIR)):
-            sudo("pip install -r requirements.txt")
+            run("pip install -r requirements.txt")
 
 
 def run_migrations():
     with prefix("source %s/bin/activate" % VENV_PATH):
         with cd("%s/%s" % (WORKING_PATH, CURRENT_DIR)):
             try:
-                sudo("./manage.py createcachetable %s" % CACHE_TABLE_NAME)
+                run("./manage.py createcachetable %s" % CACHE_TABLE_NAME)
             except:
                 pass
-            sudo("./manage.py syncdb --noinput")
-            sudo("./manage.py migrate --noinput")
+            run("./manage.py syncdb --noinput")
+            run("./manage.py migrate --noinput")
 
 
 def stop():
-    sudo("stop tcamp")
+    run("sudo /sbin/stop tcamp", pty=False)
     # with cd(HOME_PATH):
-    #     sudo("./bin/stop")
+    #     run("./bin/stop")
 
 
 def restart():
-    sudo("restart tcamp")
+    run("sudo /sbin/restart tcamp", pty=False)
     # with cd(HOME_PATH):
-    #     sudo("./bin/run")
+    #     run("./bin/run")
 
 
 def cleanup():
@@ -124,7 +123,7 @@ def cleanup():
         try:
             to_clean = dirs[PAST_RELEASES:]
             for dir in to_clean:
-                sudo("rm -rf %s" % dir)
+                run("rm -rf %s" % dir)
         except IndexError:
             pass
 
@@ -151,11 +150,11 @@ def rollback():
         dirs.reverse()
         dir = dirs[1].strip('/')
         try:
-            sudo("unlink %s" % CURRENT_DIR)
+            run("unlink %s" % CURRENT_DIR)
         except:
             pass
-        sudo("ln -s %s %s" % (dir, CURRENT_DIR))
-        sudo("rm -rf %s" % dirs[0].strip('/'))
+        run("ln -s %s %s" % (dir, CURRENT_DIR))
+        run("rm -rf %s" % dirs[0].strip('/'))
     install_dependencies()
     sync_remote_assets()
     restart()
