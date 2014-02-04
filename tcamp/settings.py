d76907e85b3685562efc4231132c042c20eb3333
Andrew Pendleton <apendleton@sunlightfoundation.com>
2014-02-04 12:46:42 -0500

    Turn on template caching.

--- tcamp/settings.py
+++ tcamp/settings.py
@@ -74,8 +74,10 @@ STATICFILES_FINDERS = (
 )
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
     'pages.templateloader.Loader',
 )
 
