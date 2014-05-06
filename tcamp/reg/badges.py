980a0b7b31207b0de80b7b4e7c9f7c091447ac70
Andrew Pendleton <apendleton@sunlightfoundation.com>
2014-05-06 13:47:47 -0400

    New, fancy zip export for tcamp attendees.

--- tcamp/reg/badges.py
+++ tcamp/reg/badges.py
@@ -1,17 +1,20 @@
 import qrcode
 from qrcode.image.svg import *
 from django.core.exceptions import PermissionDenied
+from django.views.decorators.cache import never_cache
 import shortuuid, uuid
 from cStringIO import StringIO
 from django.http import HttpResponse
 from sked.urls import CURRENT_EVENT
 from reg.models import Ticket, CouponCode
 from reg.reports import get_staff_domains
-import json, csv
+import json, csv, zipfile
 import math
 
 ### caching and auth
+# taken from http://djangosnippets.org/snippets/564/
 from django.core.cache import cache as _djcache
+from hashlib import sha1
 def cache(seconds = 900):
     def doCache(f):
         def x(*args, **kwargs):
@@ -24,6 +27,7 @@ def cache(seconds = 900):
         return x
     return doCache
 
+@cache(seconds=900)
 def key_is_staff(key):
     if not key:
         return False
@@ -55,9 +59,14 @@ def require_staff_code(func):
             raise PermissionDenied()
     return wrapped
 
-## views
+## QR code stuff
 
-def qrcode_svg(request, barcode, format):
+def qrcode_image(request, barcode, format):
+    img_data = get_qrcode_image(barcode, format)
+    mimes = {'svg': 'image/svg+xml', 'png': 'image/png'}
+    return HttpResponse(img_data, mimes[format])
+
+def get_qrcode_image(barcode, format):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -85,15 +94,19 @@ def qrcode_svg(request, barcode, format):
 </svg>
 """ % (scale, scale, buff.getvalue().replace("<?xml version='1.0' encoding='UTF-8'?>", ""))
         
-        return HttpResponse(out, content_type="image/svg+xml")
+        return out
     elif format == 'png':
         img = qr.make_image()
         img.save(buff)
 
-        return HttpResponse(buff.getvalue(), content_type="image/png")
+        return buff.getvalue()
+
+## data export stuff
 
 MAX_UUID = float(uuid.UUID('ffffffff-ffff-ffff-ffff-ffffffffffff').int)
+ICON_NAMES = ['bear.eps', 'bird.eps', 'canoe.eps', 'fire.eps', 'fish.eps', 'ivy.eps', 'robot.eps', 'sun.eps', 'tent.eps', 'tree.eps']
 
+@never_cache
 @require_staff_code
 def attendees(request, format):
     staff_domains = get_staff_domains()
@@ -114,17 +127,18 @@ def attendees(request, format):
             'first_name': ticket.first_name,
             'last_name': ticket.last_name,
             'qrcode': qrcode,
-            'qrcode_path': request.build_absolute_uri('/register/badges/qrcode/%s.png' % qrcode),
+            'qrcode_png': '%s.png' % qrcode if format == 'zip' else request.build_absolute_uri('/register/badges/qrcode/%s.png' % qrcode),
+            'qrcode_svg': '%s.svg' % qrcode if format == 'zip' else request.build_absolute_uri('/register/badges/qrcode/%s.svg' % qrcode),
             'twitter': ticket.clean_twitter,
             'organization': ticket.organization,
             'is_staff': is_staff,
-            'color': int(math.floor(10 * (qrcode_uuid.int / MAX_UUID)))
+            'icon': ICON_NAMES[int(math.floor(10 * (qrcode_uuid.int / MAX_UUID)))]
         })
     if format == 'json':
         return HttpResponse(json.dumps(out), content_type="application/json")
-    elif format == 'csv':
-        response = HttpResponse(content_type="text/csv")
-        outc = csv.DictWriter(response, ['first_name', 'last_name', 'qrcode', 'qrcode_path', 'twitter', 'organization', 'is_staff', 'color'])
+    elif format in ('csv', 'zip'):
+        csvbuff = StringIO()        
+        outc = csv.DictWriter(csvbuff, ['first_name', 'last_name', 'qrcode', 'qrcode_png', 'qrcode_svg', 'twitter', 'organization', 'is_staff', 'icon'])
         outc.writeheader()
         
         for row in out:
@@ -132,4 +146,17 @@ def attendees(request, format):
                 dict([(key, value.encode('utf8') if type(value) == unicode else value) for key, value in row.items()])
             )
 
-        return response
+        if format == 'csv':
+            return HttpResponse(csvbuff.getvalue(), content_type="text/csv")
+        elif format == 'zip':
+            zipbuff = StringIO()
+            outz = zipfile.ZipFile(zipbuff, 'w', zipfile.ZIP_DEFLATED)
+
+            outz.writestr('export.csv', csvbuff.getvalue())
+            for row in out:
+                outz.writestr(row['qrcode_png'], get_qrcode_image(row['qrcode'], 'png'), zipfile.ZIP_STORED)
+                outz.writestr(row['qrcode_svg'], get_qrcode_image(row['qrcode'], 'svg'))
+
+            outz.close()
+
+            return HttpResponse(zipbuff.getvalue(), content_type="application/zip")
--- tcamp/reg/urls.py
+++ tcamp/reg/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 from reg.views import *
-from reg.badges import qrcode_svg, attendees
+from reg.badges import qrcode_image, attendees
 
 urlpatterns = patterns('',
     url(r'^$', register),
@@ -16,8 +16,8 @@ urlpatterns = patterns('',
     url(r'^reports/volunteers.csv$', volunteer_export),
     url(r'^reports/attendees.csv$', attendee_export),
 
-    url(r'^badges/qrcode/(?P<barcode>[A-Za-z0-9]+)\.(?P<format>svg|png)$', qrcode_svg),
-    url(r'^badges/export\.(?P<format>json|csv)$', attendees),
+    url(r'^badges/qrcode/(?P<barcode>[A-Za-z0-9]+)\.(?P<format>svg|png)$', qrcode_image),
+    url(r'^badges/export\.(?P<format>json|csv|zip)$', attendees),
 )
 
 admin.autodiscover()
